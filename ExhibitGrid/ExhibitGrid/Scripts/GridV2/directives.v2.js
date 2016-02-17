ec06a83f2428716af2aca5165db5c75667eaca71
Joffpa <joffpa@gmail.com>
2016-02-17 09:11:18 -0500

    Adding js files to source control

---
+++ ExhibitGrid/ExhibitGrid/Scripts/GridV2/directives.v2.js
@@ -0,0 +1,81 @@
+var app;
+(function (app) {
+    var v2;
+    (function (v2) {
+        var directives;
+        (function (directives_1) {
+            'use strict';
+            var directives = angular.module('app.v2.directives', ['app.v2.model'])
+                .directive('exhibitRow', ['modelService', function (modelService) {
+                    return {
+                        restrict: 'A',
+                        templateUrl: '/templates/independantRow.html',
+                        controllerAs: 'rowCtrl',
+                        transclude: true,
+                        controller: ['$scope', 'modelService', app.v2.RowController]
+                    };
+                }])
+                .directive('textCell', ['modelService', function (modelService) {
+                    return {
+                        restrict: 'A',
+                        templateUrl: '/templates/v2/textCell.html',
+                        controllerAs: 'cellCtrl',
+                        transclude: true,
+                        scope: {
+                            cellVm: '='
+                        },
+                        controller: ['$scope', 'modelService', app.v2.TextCellController]
+                    };
+                }])
+                .directive('numericCell', ['modelService', function (modelService) {
+                    return {
+                        restrict: 'A',
+                        templateUrl: '/templates/v2/numericCell.html',
+                        controllerAs: 'cellCtrl',
+                        transclude: true,
+                        scope: {
+                            cellVm: '='
+                        },
+                        controller: ['$scope', 'modelService', app.v2.NumericCellController]
+                    };
+                }])
+                .directive('postitCell', ['modelService', function (modelService) {
+                    return {
+                        restrict: 'A',
+                        templateUrl: '/templates/v2/postitCell.html',
+                        controllerAs: 'cellCtrl',
+                        transclude: true,
+                        scope: {
+                            cellVm: '='
+                        },
+                        controller: ['$scope', 'modelService', app.v2.PostItCellController]
+                    };
+                }])
+                .directive('narrativeCell', ['modelService', function (modelService) {
+                    return {
+                        restrict: 'A',
+                        templateUrl: '/templates/v2/narrativeCell.html',
+                        controllerAs: 'cellCtrl',
+                        transclude: true,
+                        scope: {
+                            cellVm: '='
+                        },
+                        controller: ['$scope', 'modelService', app.v2.NarrativeCellController]
+                    };
+                }])
+                .directive('blankCell', ['modelService', function (modelService) {
+                    return {
+                        restrict: 'A',
+                        templateUrl: '/templates/v2/blankCell.html',
+                        controllerAs: 'cellCtrl',
+                        transclude: true,
+                        scope: {
+                            cellVm: '='
+                        },
+                        controller: ['$scope', 'modelService', app.v2.BlankCellController]
+                    };
+                }]);
+        })(directives = v2.directives || (v2.directives = {}));
+    })(v2 = app.v2 || (app.v2 = {}));
+})(app || (app = {}));
+//# sourceMappingURL=directives.v2.js.map
---
+++ ExhibitGrid/ExhibitGrid/Scripts/GridV2/modelService.v2.js
@@ -0,0 +1,104 @@
+/// <reference path="../typings/lodash/lodash.d.ts" />
+'use strict';
+var app;
+(function (app) {
+    var v2;
+    (function (v2) {
+        var model;
+        (function (model) {
+            var ExhibitVm = (function () {
+                function ExhibitVm(ExhibitCode) {
+                    this.ExhibitCode = ExhibitCode;
+                    this.Grids = new Array();
+                }
+                return ExhibitVm;
+            })();
+            model.ExhibitVm = ExhibitVm;
+            var GridVm = (function () {
+                function GridVm(GridCode) {
+                    this.GridCode = GridCode;
+                }
+                return GridVm;
+            })();
+            model.GridVm = GridVm;
+            var RowVm = (function () {
+                function RowVm(RowCode, Class, Text, CanCollapse, CanSelect, IsSelected) {
+                    this.RowCode = RowCode;
+                    this.Class = Class;
+                    this.Text = Text;
+                }
+                return RowVm;
+            })();
+            model.RowVm = RowVm;
+            var CellVm = (function () {
+                function CellVm(Order, Type, RowCode, ColCode, CanAddNarrative, HasNarrative) {
+                    this.Order = Order;
+                    this.Type = Type;
+                    this.RowCode = RowCode;
+                    this.ColCode = ColCode;
+                    this.CanAddNarrative = CanAddNarrative;
+                    this.HasNarrative = HasNarrative;
+                }
+                return CellVm;
+            })();
+            model.CellVm = CellVm;
+            var MockModelService = (function () {
+                function MockModelService() {
+                    this.exhibitModel = new ExhibitVm("Test Exhibit");
+                    this.addGridVm(window['gridModel'].grid);
+                }
+                MockModelService.prototype.addGridVm = function (gridVm) {
+                    this.exhibitModel.Grids.push(gridVm);
+                };
+                MockModelService.prototype.getExhibitVm = function () {
+                    return this.exhibitModel;
+                };
+                MockModelService.prototype.getGridVm = function (gridCode) {
+                    var grid = _.where(this.exhibitModel.Grids, { 'GridCode': gridCode })[0];
+                    return grid;
+                };
+                MockModelService.prototype.getRowVm = function (gridCode, rowCode) {
+                    var grid = _.where(this.exhibitModel.Grids, { 'GridCode': gridCode })[0];
+                    var row = _.where(grid.DataRows, { 'RowCode': rowCode })[0];
+                    return row;
+                };
+                MockModelService.prototype.getCellVm = function (gridCode, rowCode, colCode) {
+                    var grid = _.where(this.exhibitModel.Grids, { 'GridCode': gridCode })[0];
+                    var row = _.where(grid.DataRows, { 'RowCode': rowCode })[0];
+                    var cell = _.where(row.Cells, { 'ColCode': colCode })[0];
+                    return cell;
+                };
+                MockModelService.prototype.updateCellValue = function (gridCode, rowCode, colCode, value) {
+                    var grid = _.where(this.exhibitModel.Grids, { 'GridCode': gridCode })[0];
+                    var row = _.where(grid.DataRows, { 'RowCode': rowCode })[0];
+                    var cell = _.where(row.Cells, { 'ColCode': colCode })[0];
+                    cell.Value = value;
+                };
+                MockModelService.prototype.getCellValue = function (gridCode, rowCode, colCode) {
+                    var grid = _.where(this.exhibitModel.Grids, { 'GridCode': gridCode })[0];
+                    var row = _.where(grid.DataRows, { 'RowCode': rowCode })[0];
+                    var cell = _.where(row.Cells, { 'ColCode': colCode })[0];
+                    return cell.Value;
+                };
+                //hack to simulate total row calc 
+                MockModelService.prototype.sumAllCellsInColForTotalRow = function (gridCode, rowCode, colCode) {
+                    var grid = _.where(this.exhibitModel.Grids, { 'GridCode': gridCode })[0];
+                    var sum = 0;
+                    _.forEach(grid.DataRows, function (row) {
+                        if (row.RowCode != rowCode) {
+                            var cell = _.where(row.Cells, { 'ColCode': colCode })[0];
+                            sum += cell.Value;
+                        }
+                    });
+                    return sum;
+                };
+                return MockModelService;
+            })();
+            model.MockModelService = MockModelService;
+            var service = angular
+                .module('app.v2.model', [])
+                .service('modelService', MockModelService);
+        })(model = v2.model || (v2.model = {}));
+    })(v2 = app.v2 || (app.v2 = {}));
+})(app || (app = {}));
+//# sourceMappingURL=modelService.v2.js.map
