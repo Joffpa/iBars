e83ee23f3b369d21dee139053b85bb3626b1b287
Joffrey Pannee <joffpa@gmail.com>
2016-03-07 18:58:31 -0500

    Adding reference to Attribute

--- ExhibitGrid/ExhibitGrid/EntityDataModel/EasModel.Designer.cs
+++ ExhibitGrid/ExhibitGrid/EntityDataModel/EasModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\.2 - Workspaces\GitHub\iBars\ExhibitGrid\ExhibitGrid\EntityDataModel\EasModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\BBoss\Source\Repos\iBars\ExhibitGrid\ExhibitGrid\EntityDataModel\EasModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
