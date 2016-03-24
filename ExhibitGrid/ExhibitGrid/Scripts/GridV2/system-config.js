df4b2d2664a6ff0955b1394f12dadfb490e63703
Joffpa <joffpa@gmail.com>
2016-03-24 09:30:57 -0400

    Moving Ts files

---
+++ ExhibitGrid/ExhibitGrid/Scripts/GridV2/system-config.js
@@ -0,0 +1,14 @@
+﻿//Testing various configs
+System.config({
+    defaultJSExtensions: true,
+    packages: {
+        'angular2': {
+            defaultExtension: false
+        },
+        'rxjs': {
+            defaultExtension: false
+        }
+    }
+});
+
+System.import('/Scripts/main.js');
--- ExhibitGrid/ExhibitGrid/Scripts/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var app_component_1 = require('./app.component');
-var browser_1 = require('angular2/platform/browser');
-//var AppComponent = require('app.component').AppComponent; 
-browser_1.bootstrap(app_component_1.AppComponent);
-//# sourceMappingURL=main.js.map
